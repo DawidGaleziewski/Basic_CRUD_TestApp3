ed77e15cd248e8d231937213e7d26d3433b1d4bc
DawidGaleziewski <dawidgalsino@vp.pl>
2018-08-10 20:50:59 +0000

    basic crud done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_10_103632) do
+ActiveRecord::Schema.define(version: 2018_08_10_204024) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
